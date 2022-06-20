00983ae42ece90ae2975d17517c05d98e6839241
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-06-20 15:37:34 +0530

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.GetemployeeAttendance();
+
+        }
+
+        public void GetemployeeAttendance()
+        {
+            Random random = new Random();
+            int value = random.Next(0, 2);
+            if (value == 0)
+            {
+                Console.WriteLine("Employee is absent");
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
